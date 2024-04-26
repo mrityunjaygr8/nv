0813412d4ac6e2f9b34df438bd8f014eef9a6b39
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2024-04-26 17:43:23 +0530

    initial commit

--- config/default.nix
+++ config/default.nix
@@ -3,4 +3,9 @@
   imports = [
     ./bufferline.nix
   ];
+  colorschemes.tokyonight.enable = true;
+  plugins.rest.enable = true;
+  plugins.oil.enable = true;
+  plugins.treesitter.enable = true;
+  plugins.treesitter.ensureInstalled = ["json" "javascript" "typescript" "tsx" "yaml" "html" "css" "markdown" "markdown_inline" "bash" "lua" "vim" "dockerfile" "gitignore" "query" "vimdoc" "c" "python" "go" "rust" "sql" "nix" "http"];
 }
